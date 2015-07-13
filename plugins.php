5a219aaa60e8a4e5e2dcbfed7863cd2b2b8963fc
tcoder <tcoder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 23:23:45 +0000

    added      fonts added      fonts/solaimanlipi-webfont.eot added      fonts/solaimanlipi-webfont.svg added      fonts/solaimanlipi-webfont.ttf added      fonts/solaimanlipi-webfont.woff added      fonts/solaimanlipi-webfont.woff2 added      fonts/tcbdbanglafont.css added      plugins.php added      readme.txt added      screenshot-1.png
    git-svn-id: https://plugins.svn.wordpress.org/tcbd-bangla-font-fixer/trunk@1198396 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugins.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Plugin Name: TCBD Bangla Font Fixer
+Plugin URI: http://demos.tcoderbd.com/wordpress_plugin/tcbdbanglafont/tcbdbanglafont.zip
+Description: বাংলা ফন্ট ভেঙে যাওয়া থেকে বাঁচার উপায়
+Author: tcoderbd
+Version: 1.0
+Author URI: http://tcoderbd.com
+*/
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+
+
+// Define Plugin Directory
+define('X_PLUGIN_URL', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/' );
+
+
+
+// Custom font Call in header.
+wp_enqueue_style('tcbdbanglafont-plugin-font-css', X_PLUGIN_URL.'fonts/tcbdbanglafont.css');
+
+
+
+// Custom font for wp_head and front page body
+function tcbdbanglafont_head() {
+  ?>
+	<style type="text/css">
+		body, td, textarea, input, select {
+			font-family: "SolaimanLipi";
+		} 
+		#wpadminbar #wp-admin-bar-my-sites a.ab-item, #wpadminbar #wp-admin-bar-site-name a.ab-item #wpadminbar .quicklinks .ab-empty-item, #wpadminbar .quicklinks a, #wpadminbar .shortlink-input{
+			font-family: "SolaimanLipi";
+		} 
+	</style>
+  <?php
+}
+add_action('wp_head', 'tcbdbanglafont_head');
+
+
+
+// Custom font for wp_admin
+function tcbdbanglafont_adminfont() {
+  ?>
+	<style type="text/css">
+		body, td, textarea, input, select {
+			font-family: "SolaimanLipi";
+		} 
+		#wpadminbar #wp-admin-bar-my-sites a.ab-item, #wpadminbar #wp-admin-bar-site-name a.ab-item #wpadminbar .quicklinks .ab-empty-item, #wpadminbar .quicklinks a, #wpadminbar .shortlink-input{
+			font-family: "SolaimanLipi";
+		} 
+	</style>
+  <?php
+}
+add_action('admin_head', 'tcbdbanglafont_adminfont');
+
+
+
+// Custom font for login page
+function tcbdbanglafont_loginfont() {
+  ?>
+	<style type="text/css">
+		p {font-family: "SolaimanLipi";} 
+	</style>
+  <?php
+}
+add_action('login_head', 'tcbdbanglafont_loginfont');
